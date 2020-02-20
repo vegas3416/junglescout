4db123d29a5c288d3e22d8c7a6bfb11cb7258aa2
Jason <jasonv@indeed.com>
2020-02-20 15:46:38 -0600

    tweak test

--- src/container.ts
+++ src/container.ts
@@ -5,12 +5,12 @@ import { Dispatch } from 'react';
 import { SetActiveItem, IAction } from './data/actions/actions';
 import { IAppState } from './data/store';
 
-const mapStateToProps = (state: IAppState) => {
-  return {
-    activeItem: state.homeHub.activeItem,
-    createIsOpen: state.homeHub.createIsOpen
-  };
-};
+// const mapStateToProps = (state: IAppState) => {
+//   return {
+//     activeItem: state.homeHub.activeItem,
+//     createIsOpen: state.homeHub.createIsOpen
+//   };
+// };
 
 //This is required for it to work. Not using it but is needed
 const mapDispatchToProps = (dispatch: Dispatch<IAction>) => {
